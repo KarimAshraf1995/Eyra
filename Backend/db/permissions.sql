b561f513d54c24b939bf66afc7a188254399bb9d
Karim Ashraf Mohamed Hashish <karimashraf1995@gmail.com>
2019-03-17 22:44:34 +0200

    DB Username/Password update

--- Backend/db/permissions.sql
+++ Backend/db/permissions.sql
@@ -21,25 +21,23 @@ File author/s:
 */
 
 --create user 'default'@'localhost';
-grant select on recordings_master.device to 'default'@'localhost';
-grant select on recordings_master.instructor to 'default'@'localhost';
-grant select on recordings_master.session to 'default'@'localhost';
-grant select on recordings_master.speaker to 'default'@'localhost';
-grant select on recordings_master.speaker_info to 'default'@'localhost';
-grant select on recordings_master.token to 'default'@'localhost';
-grant select on recordings_master.recording to 'default'@'localhost';
-grant select on recordings_master.evaluation to 'default'@'localhost';
-grant select on recordings_master.evaluation_sets to 'default'@'localhost';
+grant select on recordings_master.device to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.instructor to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.session to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.speaker to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.speaker_info to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.token to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.recording to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.evaluation to 'username'@'%' IDENTIFIED BY 'password';  
+grant select on recordings_master.evaluation_sets to 'username'@'%' IDENTIFIED BY 'password';  
 
-grant insert on recordings_master.device to 'default'@'localhost';
-grant insert on recordings_master.instructor to 'default'@'localhost';
-grant insert on recordings_master.recording to 'default'@'localhost';
-grant insert on recordings_master.session to 'default'@'localhost';
-grant insert on recordings_master.speaker to 'default'@'localhost';
-grant insert on recordings_master.speaker_info to 'default'@'localhost';
-grant insert on recordings_master.evaluation to 'default'@'localhost';
+grant insert on recordings_master.device to 'username'@'%' IDENTIFIED BY 'password';  
+grant insert on recordings_master.instructor to 'username'@'%' IDENTIFIED BY 'password';  
+grant insert on recordings_master.recording to 'username'@'%' IDENTIFIED BY 'password';  
+grant insert on recordings_master.session to 'username'@'%' IDENTIFIED BY 'password';  
+grant insert on recordings_master.speaker to 'username'@'%' IDENTIFIED BY 'password';  
+grant insert on recordings_master.speaker_info to 'username'@'%' IDENTIFIED BY 'password';  
+grant insert on recordings_master.evaluation to 'username'@'%' IDENTIFIED BY 'password';  
 
-grant update on recordings_master.session to 'default'@'localhost';
-
-
+grant update on recordings_master.session to 'username'@'%' IDENTIFIED BY 'password';  
 
